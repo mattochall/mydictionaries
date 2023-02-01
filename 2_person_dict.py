0172f4ec2b803651d9f16b6c77322cef0bec4662
Matt Ochall <matt_ochall1@baylor.edu>
2023-02-01 15:32:00 -0600

    commit

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -11,12 +11,21 @@ print(person)
 
 # print out the name of the second child
 
+print(person["children"][1])
 
 # print out the name of the cat
+
+print(person["pets"]["cat"])
 
 
 # iterate through all children and print out each child
 
+for item in person["children"]:
+    print(item)
+
 
 # print out the pets in this format:
 # Type of pet: dog Name of Pet: Fido
+
+# for item in person["pets"]:
+# print(person["pets"][item])
